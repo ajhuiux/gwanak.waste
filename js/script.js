10fc97fce46fe1420e3a7774d149421ddc7ec3fe
ajhuiux <84900918+ajhuiux@users.noreply.github.com>
2022-03-07 14:12:30 +0900

    main-page

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    $(".depth2").hide();
+    $(".depth1").mouseover(function () {
+        $(this).find(".depth2").stop().slideDown(300);
+    }).mouseout(function () {
+        $(this).find(".depth2").stop().slideUp(300);
+    });
+});
+
+$(document).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll > 100) {
+      $("header > .top").addClass("scroll");
+    }
+    else if (scroll < 99) {
+      $("header > .top").removeClass("scroll");
+    }
+  });
+
+function handleOnChange(e) {
+    // 선택된 데이터 가져오기
+    const value = e.value;
+    const text = e.options[e.selectedIndex].text;
+    
+    // 데이터 출력
+    document.getElementById('text').innerText
+      = value;   
+    document.getElementById('value').innerText
+     = text;
+  }
